fe1107da803113e3a02420e85262de9b0650659a
daisuke-shimura <daisuke.s0707@icloud.com>
2025-05-11 17:27:07 +0000

    api完成 多分

--- app/controllers/fib_controller.rb
+++ app/controllers/fib_controller.rb
@@ -1,22 +1,37 @@
 class FibController < ApplicationController
   def show
-    
+    n = params[:id].to_i
+    result = fib_sequence(n)
+
+    if result.is_a?(Integer)
+      render json: { result: result}
+    else
+      render json: { ststus: 400, message: "Bad request"}, status: 400
+    end
   end
 
 
   private
   def fib_sequence(n)
     if n <= 0 || n.is_a?(String)
-      return "エラー\n1以上の整数を入力してください。"
+      return "エラー"
 
     elsif n <= 2
       return 1
 
     else
-      a = 1
-      b = 1
+      result = 0
+      fib_0 = 1
+      fib_1 = 1
 
+      (3..n).each do
+        result = fib_0 + fib_1
+        fib_0 = fib_1
+        fib_1 = result
+      end
       
+      return result
+
     end
   end
 end
