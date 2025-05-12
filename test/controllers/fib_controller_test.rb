0eaad68bff8d100d4598703e09c563df25f3dbfe
daisuke-shimura <daisuke.s0707@icloud.com>
2025-05-12 00:55:31 +0000

    テスト作成

--- test/controllers/fib_controller_test.rb
+++ test/controllers/fib_controller_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class FibControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  #正常値のテスト
+  test "正常値のテスト１" do
+    get fib_path(id: 5)
+    assert_response :success
+    json = JSON.parse(@response.body)
+    assert_equal 5, json["result"]
+  end
+
+  test "正常値のテスト２" do
+    get fib_path(id: 99)
+    assert_response :success
+    json = JSON.parse(@response.body)
+    assert_equal 218922995834555169026, json["result"]
+  end
+
+  #エラー値のテスト
+  test "負の数" do
+    get fib_path(id: -1)
+    assert_response :bad_request
+    json = JSON.parse(@response.body)
+    assert_equal "Bad request", json["message"]
+  end
+
+  test "文字列" do
+    get fib_path(id: "あ")
+    assert_response :bad_request
+    json = JSON.parse(@response.body)
+    assert_equal "Bad request", json["message"]
+  end
+
 end
